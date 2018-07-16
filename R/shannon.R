f8cbc764cd37ae5845926dc395de1f34c59c3faa
doloresderegibus <dolidd@yahoo.com>
2018-07-16 17:05:02 -0300

    va

--- R/shannon.R
+++ R/shannon.R
@@ -1,5 +1,18 @@
+#' Calculate shannin index
+#'
+#' @param species vector of species abundance
+#'
+#' @return shannon index
+#' @export
+#'
+#' @examples
+#' p<-c(10, 200, 1000, 50)
+#' shannon(p)
+#' 
 shannon <-function (species)
 {
   prop<-species/sum(species) #calcula proporciones
   -sum(prop*log(prop))#calcula shannon
 }
+
+richness <-function(species){}
